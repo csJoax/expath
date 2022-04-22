24eaeb4305f57e0d9b43727f82d908f359d2cee3
csJoax <joax@qq.com>
2022-04-22 10:44:45 +0800

    chore(setup.py): fix `readme.md` error in linux

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def find_version(*file_paths):
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as readme_file:
+with open(path.join(here, 'readme.md'), encoding='utf-8') as readme_file:
     long_description = readme_file.read()
 
 VERSION = find_version('expath', '__init__.py')
